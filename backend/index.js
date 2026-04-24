1fa32c8880a8e68df529950be755b1b99666111e
DurgaSravanthiP <sravanthi9637@gmail.com>
2026-04-24 18:50:01 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -163,7 +163,8 @@ app.post("/bfhl", (req, res) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
